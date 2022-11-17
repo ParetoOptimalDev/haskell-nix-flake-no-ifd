46609b4cd9bd29229b2d20cbe0ce7dc63dcc8171
paretoOptimalDev <pareto.optimal@mailfence.com>
2022-11-16 19:00:29 -0600

    remove nixbuild.net unsupported x86_64-darwin

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs.nixpkgs.url = "nixpkgs";
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
       nixpkgsFor = forAllSystems (system: import nixpkgs {
         inherit system;
