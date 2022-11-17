58d73c8a096238607c1eb07ecca267d2a340a66c
paretoOptimalDev <pareto.optimal@mailfence.com>
2022-11-17 10:03:00 -0600

    add deps

--- haskell-hello.nix
+++ haskell-hello.nix
@@ -1,11 +1,22 @@
-{ mkDerivation, base, lens, lib }:
+{ mkDerivation, aeson, base, cassava, conduit, containers
+, cryptonite, esqueleto, exceptions, JuicyPixels, lens, lib
+, mongoDB, persistent, persistent-mongoDB, persistent-postgresql
+, postgresql-simple, QuickCheck, servant, servant-server
+, servant-swagger, servant-swagger-ui, stm, text, time, uuid
+}:
 mkDerivation {
   pname = "try-haskell-hello-template";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base lens ];
+  executableHaskellDepends = [
+    aeson base cassava conduit containers cryptonite esqueleto
+    exceptions JuicyPixels lens mongoDB persistent persistent-mongoDB
+    persistent-postgresql postgresql-simple QuickCheck servant
+    servant-server servant-swagger servant-swagger-ui stm text time
+    uuid
+  ];
   license = lib.licenses.bsd2;
   mainProgram = "try-haskell-hello-template";
 }
