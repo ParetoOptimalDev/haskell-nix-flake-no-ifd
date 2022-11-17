d33532df3cba012ed64190c33621a4cf3b8bac09
paretoOptimalDev <pareto.optimal@mailfence.com>
2022-11-16 19:04:27 -0600

    remove broken amazonka for now
    will get compiling later

--- haskell-hello.nix
+++ haskell-hello.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, amazonka, base, lens, lib }:
+{ mkDerivation, base, lens, lib }:
 mkDerivation {
   pname = "try-haskell-hello-template";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ amazonka base lens ];
+  executableHaskellDepends = [ base lens ];
   license = lib.licenses.bsd2;
   mainProgram = "try-haskell-hello-template";
 }
