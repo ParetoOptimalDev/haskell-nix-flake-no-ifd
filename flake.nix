b61f1dfb93b18e0ffe6f302258fe50710ec264f7
paretoOptimalDev <pareto.optimal@mailfence.com>
2022-11-16 23:22:31 -0600

    disable all tests for installing haskell dependencies

--- flake.nix
+++ flake.nix
@@ -11,26 +11,34 @@
         overlays = [ self.overlay ];
       });
     in
-    {
-      overlay = (final: prev: {
-        haskell-hello = final.pkgs.haskell.packages.${ghcVersion}.callPackage ./haskell-hello.nix {  };
-      });
-      packages = forAllSystems (system: {
-         haskell-hello = nixpkgsFor.${system}.haskell-hello;
-      });
-      defaultPackage = forAllSystems (system: self.packages.${system}.haskell-hello);
-      checks = self.packages;
-      devShell = forAllSystems (system: let haskellPackages = nixpkgsFor.${system}.pkgs.haskell.packages.${ghcVersion};
-        in haskellPackages.shellFor {
-          packages = p: [self.packages.${system}.haskell-hello];
-          withHoogle = true;
-          buildInputs = with haskellPackages; [
-            haskell-language-server
-            ghcid
-            cabal-install
-          ];
-        # Change the prompt to show that you are in a devShell
-        shellHook = "export PS1='\\e[1;34mdev > \\e[0m'";
+      {
+        overlay = (final: prev: {
+          haskell-hello = final.pkgs.haskell.packages.${ghcVersion}.callPackage ./haskell-hello.nix {  };
+          haskell = prev.haskell // {
+            packageOverrides = hfinal: hprev: {
+              mkDerivation = args: hprev.mkDerivation (args // {
+                doCheck = false;
+              });
+            };
+          };
         });
-  };
+        packages = forAllSystems (system: {
+          haskell-hello = nixpkgsFor.${system}.haskell-hello;
+        });
+        defaultPackage = forAllSystems (system: self.packages.${system}.haskell-hello);
+        checks = self.packages;
+        devShell = forAllSystems (system: let haskellPackages = nixpkgsFor.${system}.pkgs.haskell.packages.${ghcVersion};
+                                          in haskellPackages.shellFor {
+                                            doCheck = false;
+                                            packages = p: [self.packages.${system}.haskell-hello];
+                                            withHoogle = true;
+                                            buildInputs = with haskellPackages; [
+                                              haskell-language-server
+                                              ghcid
+                                              cabal-install
+                                            ];
+                                            # Change the prompt to show that you are in a devShell
+                                            shellHook = "export PS1='\\e[1;34mdev > \\e[0m'";
+                                          });
+      };
 }
