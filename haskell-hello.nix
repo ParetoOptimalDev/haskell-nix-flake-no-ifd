81072233b629453476ef584206304c51d6fce363
paretoOptimalDev <pareto.optimal@mailfence.com>
2022-11-16 19:02:54 -0600

    add lens and amazonka deps

--- haskell-hello.nix
+++ haskell-hello.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, lib }:
+{ mkDerivation, amazonka, base, lens, lib }:
 mkDerivation {
   pname = "try-haskell-hello-template";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base ];
+  executableHaskellDepends = [ amazonka base lens ];
   license = lib.licenses.bsd2;
   mainProgram = "try-haskell-hello-template";
 }
